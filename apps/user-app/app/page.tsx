b9f68e8b4ec86425ec78f899933f6d37b192032a
iamindranil <indranilchakraborty692@gmail.com>
2025-02-17 01:17:24 +0530

    first commit

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,23 @@
+// import { JSX } from "react";
+
+
+
+// export default function Page():JSX.Element{
+//   return(
+//     <h1 className="text-3xl font-bold underline">
+//       Hello world!
+//     </h1>
+//   ) 
+// }
+
+
+
+const Page: React.FC=()=>{
+  return (
+    <div className="text-5xl">
+    hi there
+    </div>
+  )
+}
+
+export default Page;
